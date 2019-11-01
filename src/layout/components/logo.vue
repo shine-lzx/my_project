96e2f5033eb2b84a522bab3177b18152da7dad4e
shine-lzx <1597395958@qq.com>
2019-11-01 16:26:16 +0800

    logo

---
+++ src/layout/components/logo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    logo
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'logo',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
