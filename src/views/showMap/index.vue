8acff76560f12ed2787f10335c5f97b26036330f
shine-lzx <1597395958@qq.com>
2019-12-05 17:43:28 +0800

    地图

--- src/views/showMap/index.vue
+++ src/views/showMap/index.vue
@@ -7,10 +7,19 @@
         @keyup.enter.native="contents"
       ></el-input>
     </div>
-    <div
-      id="container"
-      class="mapContainer"
-    ></div>
+    <div class="mapBox">
+      <div
+        id="container"
+        class="mapContainer"
+      ></div>
+      <div class="mapInfo">
+        <div>{{mapData}}</div>
+        <!-- <div>{{mapData.nearestJunction}}</div>
+        <div>{{mapData.nearestPOI}}</div>
+        <div>{{mapData.nearestRoad}}</div>
+        <div>{{mapData.regeocode}}</div> -->
+      </div>
+    </div>
   </div>
 </template>
 
@@ -18,15 +27,20 @@
 export default {
   name: 'showMap',
   mounted () {
-    this.renderMap()
+    this.initMap()
   },
   data () {
     return {
       input: '',
-      map: null
+      map: null,
+      mapData: null
     }
   },
   methods: {
+    initMap () {
+      this.renderMap()
+      this.loadPositionPicker()
+    },
     renderMap () {
       /* eslint-disable no-undef */
       this.map = new AMap.Map('container', {
@@ -36,7 +50,28 @@ export default {
       })
     },
     contents () {
-      console.log('input:', this.input)
+      console.log('mapData:', this.mapData)
+    },
+    loadPositionPicker () {
+      // 拖拽选址
+      let map = this.map
+      let that = this
+      AMapUI.loadUI(['misc/PositionPicker'], function (PositionPicker) {
+        that.positionPicker = new PositionPicker({
+          mode: 'dragMap',
+          map: map
+        })
+        let positionPicker = that.positionPicker
+        positionPicker.on('success', function (positionResult) {
+          that.mapData = positionResult
+          console.log('positionResult：', that.mapData)
+        })
+        positionPicker.on('fail', function (positionResult) {
+          console.log('海上或海外无法获得地址信息')
+        })
+        // 开始选点
+        positionPicker.start()
+      })
     }
   }
 }
@@ -46,9 +81,18 @@ export default {
 .container {
   height: 100%;
   width: 100%;
-  .mapContainer {
-    width: 800px;
-    height: 500px;
+  .mapBox {
+    display: flex;
+    .mapContainer {
+      flex: 1;
+      height: 500px;
+    }
+    .mapInfo {
+      border: 1px solid red;
+      flex: 1;
+      height: 500px;
+      overflow: scroll;
+    }
   }
 }
 </style>
