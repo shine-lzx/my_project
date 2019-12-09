59fb089b80beba157047cd6226c1cf4ce4a8cc56
shine-lzx <1597395958@qq.com>
2019-12-09 11:21:15 +0800

    $listeners $attrs

--- src/views/simulationServer/components/_form/index.vue
+++ src/views/simulationServer/components/_form/index.vue
@@ -1,11 +1,29 @@
 <template>
   <div>
-    form
+    <!-- <el-input v-dfocus v-model="input" placeholder="请输入内容"></el-input> -->
+    <!-- <input :value="value" v-bind="$attrs" @input="$emit('input', $event.target.value)" /> -->
+    <input :value="value" v-bind="$attrs" v-on="listeners" />
   </div>
 </template>
 
 <script>
 export default {
-  name: '_form'
+  name: 'myForm',
+  data () {
+    return {
+      value: ''
+    }
+  },
+  computed: {
+    listeners () {
+      return {
+        ...this.$listeners,
+        input: event => {
+          // console.log(event.target.value)
+          this.$emit('input', event.target.value)
+        }
+      }
+    }
+  }
 }
 </script>
--- src/views/simulationServer/index.vue
+++ src/views/simulationServer/index.vue
@@ -1,17 +1,23 @@
 <template>
   <div>
     <div>Mock</div>
+    <myForm @input="input" />
     <el-button @click="myClick">myClick</el-button>
     <el-button @click="cancelClick">cancelClick</el-button>
   </div>
 </template>
 
 <script>
+// import './components/componentRegister.js'
 import axios from 'axios'
 import _console from '../../mixins/_console'
+import myForm from './components/_form/index'
 export default {
   name: 'simulationServer',
   mixins: [_console],
+  components: {
+    myForm
+  },
   created () {
     axios.post('/datas').then(res => {
       console.log(res)
@@ -40,6 +46,9 @@ export default {
     },
     cancelClick () {
       this.cancel()
+    },
+    input (val) {
+      console.log('子组件', val)
     }
   }
 }
